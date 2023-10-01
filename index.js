c88461145ace46078b7a669cbefa7ab5ec6f3703
mohan <patidarmohan0907@gmail.com>
2023-10-01 16:55:56 +0530

    added socket-io

--- index.js
+++ index.js
@@ -4,12 +4,25 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({origin: 'http://localhost:3000'}))
+const isDev = app.settings.env === 'development'
+const URL = isDev ? 'http://localhost:3000' : 'https://online-drawing-web-8ydezhse5-mohan0907.vercel.app/'
+app.use(cors({origin: URL}))
 const httpServer = createServer(app);
-const io = new Server(httpServer, { cors: 'http://localhost:3000' });
+const io = new Server(httpServer, { cors: URL });
 
 io.on("connection", (socket) => {
-  console.log("server connected")
+    console.log("server connected")
+    socket.on('beginPath', (arg) => {
+        socket.broadcast.emit('beginPath',arg)
+    })
+
+    socket.on('drawline', (arg) => {
+        socket.broadcast.emit('drawline',arg)
+    })
+
+    socket.on('changeConfig', (arg) => {
+        socket.broadcast.emit('changeConfig',arg)
+    })
 });
 
 httpServer.listen(5000);
